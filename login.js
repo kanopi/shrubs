67f7a71c44312b6d37635f057c3f0f362c50bf64
Andrew Sipple <asipple1@gmail.com>
2024-11-06 08:20:03 -1000

    Use session cookie to check if the user is logged in

--- login.js
+++ login.js
@@ -3,32 +3,33 @@
 // cy.login('username', 'password'); // login as a specific user.
 
 Cypress.Commands.add('login', (usernameParam = '', password = '') => {
-    cy.logout()
-    cy.visit('/user/login')
-    let username = '';
-    let default_user = false;
-    if (usernameParam === '' && password === '') {
-      username = 'cypress';
-      default_user = true;
-    }
-    else {
-      username = usernameParam;
-    }
+  cy.logout();
+  cy.visit("/user/login");
+  let username = "";
+  let default_user = false;
+  if (usernameParam === "" && password === "") {
+    username = "cypress";
+    default_user = true;
+  } else {
+    username = usernameParam;
+  }
 
-    if (default_user) {
-        cy.get('form.user-login-form #edit-name').type(username);
-        cy.get('form.user-login-form #edit-pass').type('cypress');
-    }
-    else {
-        cy.get('form.user-login-form #edit-name').type(username);
-        cy.get('form.user-login-form #edit-pass').type(password);
-    }
+  if (default_user) {
+    cy.get("form.user-login-form #edit-name").type(username);
+    cy.get("form.user-login-form #edit-pass").type("cypress");
+  } else {
+    cy.get("form.user-login-form #edit-name").type(username);
+    cy.get("form.user-login-form #edit-pass").type(password);
+  }
 
-    // Login.
-    cy.get('form.user-login-form .form-submit').click();
-    cy.wait(500) // Wait for the UI to catch up.
+  // Login.
+  cy.get("form.user-login-form .form-submit").click();
+  cy.wait(500); // Wait for the UI to catch up.
 
-    // Check user page loads.
-    cy.get('main').contains(username)
-    cy.get('main').contains('Member for')
+  // Check user session cookie is set after logging in.
+  cy.getCookies()
+    .should("have.length", 1)
+    .then((cookies) => {
+      expect(cookies[0].name).to.include("SESS");
+    });
 })
