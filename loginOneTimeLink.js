bfceb9f3427d1a11e538f7f813088cc90c6897c0
Jim Birch <5177009+thejimbirch@users.noreply.github.com>
2025-05-31 06:52:01 -0400

    Update and rename loginAsUser.js to loginOneTimeLink.js

--- loginOneTimeLink.js
+++ loginOneTimeLink.js
@@ -1,7 +1,7 @@
 /**
  * Custom Cypress command to log in as a specific Drupal user.
  * 
- * Usage: cy.loginAsUser('admin')
+ * Usage: cy.loginOneTimeLink('admin')
  * 
  * This command uses Drush to generate a one-time login link for the specified user
  * and automatically visits that link to authenticate the session. It depends on
@@ -9,7 +9,7 @@
  * 
  * @param {string} username - The username of the Drupal user to log in as
  */
-Cypress.Commands.add('loginAsUser', (username) => {
+Cypress.Commands.add('loginOneTimeLink', (username) => {
   cy.drush(`uli --name=${username}`)
   cy.get('@drushOutput').then((link) => {
     cy.visit(link)
