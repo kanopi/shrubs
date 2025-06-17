8704586679f06c7de219377c361b3cf52e08e406
Anne Bonham <anne.bonham@kanopistudios.com>
2025-06-16 19:59:53 -0700

    tw29821365 - adds background option to drush command

--- drush.js
+++ drush.js
@@ -6,25 +6,31 @@ Cypress.Commands.add('drush', (command, options = {}) => {
 
   let exec_command = '';
 
+  const isBackground = options.background === true;
+  const suffix = isBackground ? ' &' : '';
+
   if (Cypress.env('DRUSH_IS_DOCKSAL')) {
-    exec_command = 'fin drush '  + command;
+    exec_command = 'fin drush '  + command + suffix;
   }
 
   if (Cypress.env('DRUSH_IS_LANDO')) {
-    exec_command = 'lando drush '  + command;
+    exec_command = 'lando drush '  + command + suffix;
   }
 
   if (Cypress.env('DRUSH_IS_DDEV')) {
-    exec_command = 'ddev drush '  + command;
+    exec_command = 'ddev drush '  + command + suffix;
   }
 
   // In the format of PANTHEON_SITE_ID.ENVIRONMENT_ID
   if(Cypress.env('DRUSH_IS_PANTHEON')) {
     // Passing URI as well because drush will return HTTP links by default and not HTTPS which can cause tests to fail.
-    exec_command = 'ssh -T ' + Cypress.env('DRUSH_IS_PANTHEON') + '@appserver.' + Cypress.env('DRUSH_IS_PANTHEON') + '.drush.in -p 2222 -o "StrictHostKeyChecking=no" -o "AddressFamily inet" "drush --uri=' + Cypress.config('baseUrl') +  ' ' + command + '"';
+    exec_command = 'ssh -T ' + Cypress.env('DRUSH_IS_PANTHEON') +
+      '@appserver.' + Cypress.env('DRUSH_IS_PANTHEON') + '.drush.in -p 2222 ' +
+      '-o "StrictHostKeyChecking=no" -o "AddressFamily inet" ' +
+      `"drush --uri=${Cypress.env('CYPRESS_BASE_URL')} ${command}${suffix}"`;
   }
 
-  cy.exec(exec_command, options).then((result) => {
+  cy.exec(exec_command, { ...options, failOnNonZeroExit: false }).then((result) => {
     cy.log(result.stdout);
     cy.wrap(result.stdout).as('drushOutput');
   })
